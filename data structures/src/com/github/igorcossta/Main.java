eb8744402577fa7bc351f10182955b7d525511f1
Igor Cossta <igorcossta21@gmail.com>
2021-03-08 17:19:31 -0300

    data structure: array v1

--- data structures/src/com/github/igorcossta/Main.java
+++ data structures/src/com/github/igorcossta/Main.java
@@ -1,5 +1,44 @@
 package com.github.igorcossta;
 
-public class Main {
+import com.github.igorcossta.array.Vetor;
 
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // teste de adição de elemento
+        Vetor<Integer> array0 = new Vetor<>(2);
+
+        array0.add(1);
+        array0.add(2);
+
+        array0.add(3); // aumenta a capacidade do vetor
+        System.out.println(array0.size() + " " + array0.length()); //   elementos = 3           tamanho vetor = 4
+
+        System.out.println("");
+
+        // teste de remoção de elemento
+        Vetor<String> array = new Vetor<>(10);
+
+        array.add("string 1");
+        array.add("string 2");
+        array.add("string 3");
+
+        System.out.println("Pré remoção -> Elementos = " + array.size() + " Tamanho do vetor = " + array.length());
+        System.out.print(array.valueOf(0) + ", " + array.valueOf(1) + ", " + array.valueOf(2) + "\n");
+
+        array.remove(0);
+
+        System.out.println("Pós remoção -> Elementos = " + array.size() + " Tamanho do vetor = " + array.length());
+        System.out.print(array.valueOf(0) + ", " + array.valueOf(1) + ", " + array.valueOf(2) + "\n");
+
+        System.out.println("");
+
+        // teste de retornar índice ao buscar por elemento
+        System.out.println("IndexOf Test -> " + array.indexOf("string 2"));
+
+        // teste de retornar valor do índice
+        System.out.println("ValueOf Test -> " + array.valueOf(0));
+    }
 }
--- data structures/src/com/github/igorcossta/array/Vetor.java
+++ data structures/src/com/github/igorcossta/array/Vetor.java
@@ -1,4 +1,76 @@
 package com.github.igorcossta.array;
 
-public class Vetor {
+public class Vetor<T> {
+    private T array[];
+    private int size;
+
+    public Vetor(int capacity) {
+        if (capacity != 0) {
+            this.array = (T[]) new Object[capacity];
+            this.size = 0;
+        } else {
+            throw new ArrayStoreException("The array size must be greater than zero.");
+        }
+    }
+
+    public boolean add(T object) {
+        increase();
+        if (size < array.length) {
+            this.array[size] = object;
+            this.size++;
+            return true;
+        }
+        return false;
+    }
+
+        private void increase() {
+        if (this.size == this.array.length) {
+            T[] newArray = (T[]) new Object[array.length * 2];
+            for (int i = 0; i < array.length; i++) {
+                newArray[i] = array[i];
+            }
+            this.array = newArray;
+        }
+    }
+
+    public int indexOf(T object) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(object)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public T valueOf(int index) {
+        return array[index];
+    }
+
+    public void remove(int index) {
+        if (index > array.length || index < 0) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        for (int i = index; i < array.length - 1; i++) {
+            this.array[i] = array[i + 1];
+        }
+        size--;
+    }
+
+    /***
+     * method to return the size of the array
+     * @return array size
+     */
+
+    public int length() {
+        return array.length;
+    }
+
+    /***
+     * method to return the number of elements
+     * @return number of elements
+     */
+
+    public int size() {
+        return size;
+    }
 }
